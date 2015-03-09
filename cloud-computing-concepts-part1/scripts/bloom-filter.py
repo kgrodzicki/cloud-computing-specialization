759b1734e82ee3b748ed857b8a635008abb6dd2e
Krzysztof Grodzicki <kgrodzicki@gmail.com>
2015-03-09 23:00:37 +0100

    fix bloom filter script

--- cloud-computing-concepts-part1/scripts/bloom-filter.py
+++ cloud-computing-concepts-part1/scripts/bloom-filter.py
@@ -3,13 +3,16 @@ __author__ = 'grokrz'
 m = 32
 
 
-def hash_function(x, i, m):
-    return ((pow(x, 2) * pow(x, 3)) * i) % m
+def hash_function(x, i):
+    return ((pow(x, 2) + pow(x, 3)) * i) % m
 
 
-def show_bits_set_to_1(val):
+def show_bits_set_to_1(value):
     for i in range(1, 4):
-        print "Bit set to 1: " + str(hash_function(val, i, m))
+        print "Bit set to 1: " + str(hash_function(value, i))
 
 
-show_bits_set_to_1(2013)
+values = [2010, 2013, 2007]
+
+for val in values:
+    show_bits_set_to_1(val)
