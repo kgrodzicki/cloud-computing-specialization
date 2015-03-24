5ad68bba64fd7b0fc94671081dc87786b4348ff5
Krzysztof Grodzicki <kgrodzicki@gmail.com>
2015-03-24 19:31:38 +0100

    add script for maekawa mutual exclusion

---
+++ cloud-computing-concepts-part2/scripts/maekawa.py
@@ -0,0 +1,26 @@
+__author__ = 'grokrz'
+
+P1 = ['P1', 'P2', 'P4', 'P6']
+P2 = ['P1', 'P2', 'P3', 'P4']
+P3 = ['P1', 'P2', 'P3', 'P5']
+P4 = ['P1', 'P2', 'P4', 'P5']
+P5 = ['P3', 'P4', 'P5', 'P6']
+P6 = ['P1', 'P3', 'P5', 'P6']
+
+ALL = [P1, P2, P3, P4, P5, P6]
+
+
+
+def check(processes):
+    result = []
+    for p0 in processes:
+        for p1 in processes:
+            if p0 != p1:
+                print "checking intersection between {} and {}".format(p0, p1)
+                intersection = set(p0).intersection(p1)
+                print intersection
+                result.append(intersection != set())
+    return result
+
+
+print "has no intersection = {}".format(False in check(ALL))
