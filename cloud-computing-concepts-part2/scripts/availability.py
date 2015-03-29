b85407814f5614860f3d38f5e330b2bca4a588b2
Krzysztof Grodzicki <kgrodzicki@gmail.com>
2015-03-29 15:14:38 +0200

    fix availability script

--- cloud-computing-concepts-part2/scripts/availability.py
+++ cloud-computing-concepts-part2/scripts/availability.py
@@ -2,13 +2,15 @@ __author__ = 'grokrz'
 
 availability_of_single_replica = 0.85
 server_failure_probability = 1 - availability_of_single_replica
-max_number_of_servers = 7
+max_number_of_servers = 8
+
+print server_failure_probability
 
 
 def get_availability_of_object(nr_of_servers):
     if nr_of_servers == 1:
-        return (1 - server_failure_probability) * 100
-    return (1 - pow(server_failure_probability, nr_of_servers)) * 100
+        return 1 - server_failure_probability
+    return 1 - pow(server_failure_probability, nr_of_servers)
 
 
 for i in range(1, max_number_of_servers):
