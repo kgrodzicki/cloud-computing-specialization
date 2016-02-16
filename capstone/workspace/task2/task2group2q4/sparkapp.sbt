b7cd76358de8a222b169a9bb92052f78c3f92a9e
Krzysztof Grodzicki <kgrodzicki@gmail.com>
2016-02-16 19:08:33 +0100

    save state and add cassandra integration

--- capstone/workspace/task2/task2group2q4/sparkapp.sbt
+++ capstone/workspace/task2/task2group2q4/sparkapp.sbt
@@ -1,7 +1,21 @@
-name := "spark app"
+name := "task2group2q4"
 
 version := "1.0"
 
 scalaVersion := "2.10.5"
 
+mainClass in Compile := Some("App")
+
 libraryDependencies += "org.apache.spark" %% "spark-core" % "1.6.0"
+
+libraryDependencies += "org.apache.spark" % "spark-streaming-kafka_2.10" % "1.6.0"
+
+libraryDependencies += "org.apache.spark" % "spark-streaming_2.10" % "1.6.0"
+
+libraryDependencies += "com.datastax.spark" %% "spark-cassandra-connector" % "1.5.0-RC1"
+
+mergeStrategy in assembly <<= (mergeStrategy in assembly) { (old) => {
+  case PathList("META-INF", xs@_*) => MergeStrategy.discard
+  case x => MergeStrategy.first
+}
+}
--- capstone/workspace/task2/task2group2q4/src/main/scala/App.scala
+++ capstone/workspace/task2/task2group2q4/src/main/scala/App.scala
@@ -1,9 +1,20 @@
-import org.apache.spark.{SparkConf, SparkContext}
+import java.lang.System._
+
+import com.datastax.spark.connector.cql.CassandraConnector
+import com.datastax.spark.connector.{SomeColumns, _}
+import kafka.serializer.StringDecoder
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.kafka.KafkaUtils
+import org.apache.spark.streaming.{Duration, Seconds, StreamingContext}
+
+import scala.collection.immutable
 
 /**
   * @author <a href="mailto:kgrodzicki@gmail.com">Krzysztof Grodzicki</a> 13/02/16.
   */
 object App {
+
 
   object Model {
 
@@ -18,13 +29,51 @@ object App {
   }
 
   def main(args: Array[String]) {
-    val inputFile = "./src/main/resources/input.txt"
-    val conf = new SparkConf().setAppName("Spark Task 2 group 2 question 4")
-    val sc = new SparkContext(conf)
-    val data = sc.textFile(inputFile, 2).cache()
+
+    if (args.length < 3) {
+      err.println(
+        s"""
+           | Usage: App <brokers> <topics> <brokers>
+           |  <brokers> is a list of one or more Kafka brokers
+           |  <topics> is a list of one or more kafka topics to consume from
+           |  <brokers> is a list of one or more Cassandra brokers
+           |
+        """.stripMargin)
+      exit(1)
+    }
+
+    val Array(kafkaBrokers, topics, cassandraBrokers) = args
+    val batchDuration: Duration = Seconds(5)
+    val cassandraHost: String = cassandraBrokers.split(":")(0)
+    val cassandraPort: String = cassandraBrokers.split(":")(1)
+    val keepAlliveMs = batchDuration.+(Seconds(5)).milliseconds // keep connection alive between batch sizes
+
+    val conf = new SparkConf(true)
+      .set("spark.cassandra.connection.host", cassandraHost)
+      .set("spark.cassandra.connection.port", cassandraPort)
+      .set("spark.cassandra.auth.username", "cassandra")
+      .set("spark.cassandra.auth.password", "cassandra")
+      .set("spark.cassandra.connection.keep_alive_ms", s"$keepAlliveMs")
+      .setAppName("Spark Task 2 group 2 question 4")
+
+    /** Creates the keyspace and table in Cassandra. */
+    CassandraConnector(conf).withSessionDo { session =>
+      session.execute(s"CREATE KEYSPACE IF NOT EXISTS capstone WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1}")
+      session.execute(s"CREATE TABLE IF NOT EXISTS capstone.meanarrdelay(origin text, dest text, mean text, PRIMARY KEY(origin, dest));")
+      session.execute(s"TRUNCATE capstone.meanarrdelay")
+    }
+
+    val ssc = new StreamingContext(conf, batchDuration)
+    ssc.checkpoint("checkpoint")
+
+    val topicsSet = topics.split(",").toSet
+    val kafkaParams = Map[String, String]("metadata.broker.list" -> kafkaBrokers)
+
+    val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet)
+    val lines: DStream[String] = messages.map(_._2)
 
     import Model.OriginDestArrDelay
-    val result = data.map { line: String =>
+    val result: DStream[(String, String, Int)] = lines.map { line: String =>
       // split each line
       line.split(",") match {
         case Array(origin, dest, arrDelay) => OriginDestArrDelay(origin, dest, parseDouble(arrDelay))
@@ -36,22 +85,35 @@ object App {
       case _ => false
     }).map {
       // make a tuple for each origin dest and arrDelay
-      case a@OriginDestArrDelay(origin, dest, arrDelay) => (origin, (dest, arrDelay.get))
-    }.groupByKey().map(a => {
-      val origin: String = a._1
-      val dests: Iterable[(String, Double)] = a._2
+      case a@OriginDestArrDelay(origin, dest, arrDelay) => (origin, (dest, arrDelay.get.toInt))
+    }.groupByKey()
+      .updateStateByKey(updateState)
+      .map(a => {
+        val origin: String = a._1
+        val dests: Seq[(String, Int)] = a._2
 
-      val originDestArrDelayMean = dests.groupBy(_._1).map(each => {
-        val dest = each._1
-        val delays = each._2.map(_._2)
-        val meanArrDelay: Double = delays.sum / delays.size
-        (origin, dest, meanArrDelay)
-      })
+        dests.groupBy(_._1).map(each => {
+          val dest = each._1
+          val delays = each._2.map(_._2)
+          val meanArrDelay: Double = delays.sum / delays.size
 
-      originDestArrDelayMean
-    })
+          (origin, dest, meanArrDelay.toInt)
+        })
+      }).flatMap(_.map(a => a))
 
-    result.foreach(println)
+    result.foreachRDD(_.saveToCassandra("capstone", "meanarrdelay", SomeColumns("origin", "dest", "mean")))
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+  def updateState(newValues: Seq[Iterable[(String, Int)]], runningCount: Option[Iterable[(String, Int)]]): Option[Seq[(String, Int)]] = {
+    runningCount match {
+      case Some(rc) =>
+        Some(newValues.flatten ++ rc)
+      case _ =>
+        Some(newValues.flatten)
+    }
   }
 
 }
